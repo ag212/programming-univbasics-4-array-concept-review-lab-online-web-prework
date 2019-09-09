e69dcec21794ea4f8e6f1342ee6dda71c2cc7686
Abraham Galva <ag212@icloud.com>
2019-09-09 00:45:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,5 @@
 def find_element_index(array, value_to_find)
-  counter=0
-  array=[scale,scale2]
-  while array[counter] do
-    puts array[counter]
-  counter +=1
+  
 end
 
 def find_max_value(array)
