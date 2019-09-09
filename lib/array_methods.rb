fc7d005d192b4711da6200e8e123b079b1314fe8
Abraham Galva <ag212@icloud.com>
2019-09-09 00:50:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,10 @@
 def find_element_index(array, value_to_find)
-  counter=0
+  count =0
   while count < array.length do
-    array.index(value_to_find)
-  counter += 1
+    if array[count] == value_to_find
+      return count
+    end
+  count += 1
 end
 nil 
 
